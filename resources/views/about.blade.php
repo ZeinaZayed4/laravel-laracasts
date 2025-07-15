06eb29c7fcf15f48eb40fa34c2d2e7abd01b2402
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-07-16 02:59:35 +0300

    Make it pretty using Tailwind CSS

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,11 @@
 <x-layout>
+    <x-slot:title>
+        About Page
+    </x-slot:title>
+
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+
     <h1>Hello from the About Page.</h1>
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,11 @@
 <x-layout>
+    <x-slot:title>
+        Contact Page
+    </x-slot:title>
+
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+
     <h1>Hello from the Contact Page.</h1>
 </x-layout>
