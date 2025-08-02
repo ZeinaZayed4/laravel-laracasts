b9ce09860818b4db35e3d6ea2d4f80e63991cfa1
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-08-02 23:48:41 +0300

    Day 23
    6 steps to authorization mastery

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -11,7 +11,9 @@
 
     <p>This job pays {{ $job->salary }} per year.</p>
 
-    <p class="mt-6">
-        <x-button href="{{ $job->id }}/edit">Edit Job</x-button>
-    </p>
+    @can('edit', $job)
+        <p class="mt-6">
+            <x-button href="{{ $job->id }}/edit">Edit Job</x-button>
+        </p>
+    @endcan
 </x-layout>
